b734a99c91dc00c509f448a39a48de810f5166f4
DayRise1 <lego79796@gmail.com>
2026-05-08 15:47:18 -0500

    Update emails.js

--- emails.js
+++ emails.js
@@ -1,7 +1,7 @@
 ﻿const emails = [
     {
     id: "ADB Update 5/2/2026",
-    subject: "We solved one problem (and found a new one) - ABD Update 2",
+    subject: "We solved one problem (and found a new one) - ADB Update 2",
     sender: "<b>Jonas Webber</b> (asher@bostonshairdos.com)",
     date: "Apr 11",
     unread: true,
